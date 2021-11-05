7e49245be223d70127f91226737b1debd8e76190
AshishKaflu <ashish2054@hotmail.com>
2021-11-05 11:54:47 +1100

    Added Speed for more natural Gameplay

--- 13730561_Assess3./Assets/Scripts/PacStudentController.cs
+++ 13730561_Assess3./Assets/Scripts/PacStudentController.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class PacStudentController : MonoBehaviour
 {
+
+    public float Speed = 10f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,6 @@ public class PacStudentController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Input.GetAxis("Horizontal") * Time.deltaTime,Input.GetAxis("Vertical") * Time.deltaTime,0);
+        transform.Translate(Input.GetAxis("Horizontal") * Time.deltaTime * Speed , Input.GetAxis("Vertical") * Time.deltaTime * Speed ,0);
     }
 }
