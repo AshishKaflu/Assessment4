abd2c1dbb7d17f9276fc658ad751853cb21f467e
AshishKaflu <ashish2054@hotmail.com>
2021-11-05 15:46:20 +1100

    Player Movement

--- 13730561_Assess3./Assets/Scripts/PacStudentController.cs
+++ 13730561_Assess3./Assets/Scripts/PacStudentController.cs
@@ -4,24 +4,26 @@ using UnityEngine;
 
 public class PacStudentController : MonoBehaviour
 {
-
     public float Speed = 04f;
-    private KeyCode currentInput;
-    private KeyCode lastInput;
-
 
     // Start is called before the first frame update
     void Start()
     {
-        
+      
     }
 
     // Update is called once per frame
     void Update()
     {
 
-       float horizontalmovement = Input.GetAxis("Horizontal") * Time.deltaTime * Speed;
-       float verticalmovement = Input.GetAxis("Vertical") * Time.deltaTime * Speed;
-       transform.Translate(horizontalmovement, verticalmovement,0);
+
+        float horizontalmovement = Input.GetAxisRaw("Horizontal") * Time.deltaTime * Speed;
+        float verticalmovement = Input.GetAxisRaw("Vertical") * Time.deltaTime * Speed;
+        transform.Translate(horizontalmovement, verticalmovement, 0);
+
     }
+
+
+    
+
 }
