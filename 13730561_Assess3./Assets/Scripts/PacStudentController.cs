aaaa618f3e2b2723c5a1fb92e03768a627a9a9ed
AshishKaflu <ashish2054@hotmail.com>
2021-11-05 16:23:51 +1100

    PacStudent Facing the moving Direction

--- 13730561_Assess3./Assets/Scripts/PacStudentController.cs
+++ 13730561_Assess3./Assets/Scripts/PacStudentController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PacStudentController : MonoBehaviour
 {
     public float Speed = 04f;
+    public Animator animatorController;
 
     // Start is called before the first frame update
     void Start()
@@ -16,6 +17,28 @@ public class PacStudentController : MonoBehaviour
     void Update()
     {
 
+
+        //float horizontalmovement = Input.GetAxisRaw("Horizontal") * Time.deltaTime * Speed;
+        //float verticalmovement = Input.GetAxisRaw("Vertical") * Time.deltaTime * Speed;
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            animatorController.SetTrigger("DownParam");
+        }
+
+        if(Input.GetKeyDown(KeyCode.W)) {
+            animatorController.SetTrigger("UpParam");
+        }
+
+        if(Input.GetKeyDown(KeyCode.A))
+        {
+            animatorController.SetTrigger("LeftParam");
+        }
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            animatorController.SetTrigger("RightParam");
+        }
 
         float horizontalmovement = Input.GetAxisRaw("Horizontal") * Time.deltaTime * Speed;
         float verticalmovement = Input.GetAxisRaw("Vertical") * Time.deltaTime * Speed;
